5c3cdaae29a34f71b88d788dfb080a0fb8d83fc7
Adem ÖZÇELİK <63512474+AdemOzcelik@users.noreply.github.com>
2025-12-12 11:48:05 +0300

    Add placeholder function and Logger class

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+def placeholder():
+    """
+    Placeholder function.
+    """
+    pass
+
+
+class Logger:
+    """Simple placeholder logger class."""
+
+    def __init__(self):
+        pass
+
+    def log(self, message):
+        print(message)
